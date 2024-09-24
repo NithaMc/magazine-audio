94da9d18dd3c694fd6263db93a81901fa60db79b
NithaMc <nithamc114@gmail.com>
2024-09-24 08:14:41 -0700

    first commit

--- src/Components/ScannedAudioPage.jsx
+++ src/Components/ScannedAudioPage.jsx
@@ -4,7 +4,10 @@ import AudioPlayer from './AudioPlayer.jsx';
 
 
 const audioFiles = {
-  1: '/audios/1_lv_0_20240904060756_320kbps (1).mp3',
+  1: '/audios/1_lv_0_20240904060756_320kbps (1).mp3',git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/NithaMc/magazine-audio.git
+  git push -u origin main
   2: '/audios/STREEKAL.mp3',
   3: '/audios/AUD-20240923-WA0014.mp3',
   4: '/audios/AUD-20240923-WA0013.mp3',
